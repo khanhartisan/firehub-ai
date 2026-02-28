6864057d288d51e89b97647f46b90870eff0901c
khanh.artisan <khanh.artisan@gmail.com>
2026-02-28 12:50:02 +0700

    update EntityCountable.php concern

--- app/Models/Concerns/EntityCountable.php
+++ app/Models/Concerns/EntityCountable.php
@@ -6,6 +6,7 @@ use App\Enums\EntityType;
 use App\Enums\ScrapingStatus;
 use App\Models\EntityCount;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
 trait EntityCountable
@@ -19,22 +20,23 @@ trait EntityCountable
 
     public function adjustEntityCount(EntityType $entityType, ScrapingStatus $scrapingStatus, int $delta): bool
     {
-        /** @var EntityCount $record */
-        $record = $this->entityCounts()
-            ->where('entity_type', $entityType)
-            ->where('scraping_status', $scrapingStatus)
-            ->first();
-
-        if ($record) {
-            return $record->increment('count', $delta) !== false;
+        if ($delta === 0) {
+            return true;
         }
 
-        return $this->entityCounts()->create([
-            'id' => (string) Str::ulid(),
-            'countable_type' => $this->getMorphClass(),
-            'entity_type' => $entityType,
-            'scraping_status' => $scrapingStatus,
-            'count' => max(0, $delta),
-        ]) !== null;
+        return !!EntityCount::query()->upsert([
+            [
+                'id' => strtolower(Str::ulid()->toString()),
+                'countable_type' => $this->getMorphClass(),
+                'countable_id' => $this->getKey(),
+                'entity_type' => $entityType,
+                'scraping_status' => $scrapingStatus,
+                'count' => $delta
+            ]
+        ], [
+            'countable_type', 'countable_id', 'entity_type', 'scraping_status',
+        ], [
+            'count' => DB::raw('count '.($delta > 0 ? '+' : '-').abs($delta))
+        ]);
     }
 }

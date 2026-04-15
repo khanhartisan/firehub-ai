e150e56d022a6d1614d7d68f79e559577c88226c
khanh.artisan <khanh.artisan@gmail.com>
2026-04-15 17:16:45 +0700

    add `getIdeaForge` and `setIdeaForge` methods to `Synthesizer` interface

--- app/Contracts/Synthesizer/Synthesizer.php
+++ app/Contracts/Synthesizer/Synthesizer.php
@@ -4,10 +4,15 @@ namespace App\Contracts\Synthesizer;
 
 use App\Contracts\Synthesizer\Author\Author;
 use App\Contracts\Synthesizer\BriefBuilder\BriefBuilder;
+use App\Contracts\Synthesizer\IdeaForge\IdeaForge;
 use App\Contracts\Synthesizer\OutlineBuilder\OutlineBuilder;
 
 interface Synthesizer
 {
+    public function getIdeaForge(): IdeaForge;
+
+    public function setIdeaForge(IdeaForge $ideaForge): static;
+
     public function setBriefBuilder(BriefBuilder $builder): static;
 
     public function getBriefBuilder(): BriefBuilder;

ee193774263e8dc33eb4ca55bc14b8113e90e975
khanh.artisan <khanh.artisan@gmail.com>
2026-04-20 17:35:37 +0700

    enhance `KeywordResearchJob` to include `forceResearch` option, validity checks, and initial implementation of `SearchOptions` setup; improve status handling and lock management

--- app/Jobs/KeywordResearchJob.php
+++ app/Jobs/KeywordResearchJob.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Contracts\SearchEngine\SearchOptions;
 use App\Enums\KeywordStatus;
 use App\Enums\Queue;
 use App\Jobs\Concerns\HasManualLock;
@@ -21,12 +22,14 @@ class KeywordResearchJob implements ShouldQueue, ShouldBeUniqueUntilProcessing
 
     public int $uniqueFor = 300;
 
+    public int $validityDuration = 86400;
+
     protected Keyword $keyword;
 
     /**
      * Create a new job instance.
      */
-    public function __construct(Keyword $keyword)
+    public function __construct(Keyword $keyword, protected bool $forceResearch = false)
     {
         $this->keyword = $keyword->withoutRelations();
 
@@ -43,10 +46,36 @@ class KeywordResearchJob implements ShouldQueue, ShouldBeUniqueUntilProcessing
      */
     public function handle(): void
     {
+        if (!$lock = $this->getManualLock() or !$lock->get()) {
+            return;
+        }
+
         $keyword = $this->keyword;
+
+        // Mark as researched
+        if (!$this->forceResearch
+            and $keyword->researched_at
+            and abs(now()->diffInSeconds($keyword->researched_at)) <= $this->validityDuration
+        ) {
+            $keyword->status = KeywordStatus::RESEARCHED;
+            $keyword->save();
+            return;
+        }
+
+        // Mark keyword as researching
         $keyword->status = KeywordStatus::RESEARCHING;
         $keyword->save();
 
-        // TODO: Implement keyword researching
+        // Build the search options
+        $searchOptions = new SearchOptions();
+        $searchOptions->setLanguage($keyword->language);
+        $searchOptions->setCountry($keyword->country);
+
+        // Perform search
+        try {
+            // TODO: Continue implementing
+        } finally {
+            $lock->release();
+        }
     }
 }

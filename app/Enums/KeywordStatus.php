c80af2c2f317bc4dd8815dc4b4cec12692fbb819
khanh.artisan <khanh.artisan@gmail.com>
2026-04-21 16:13:54 +0700

    add helper methods to `KeywordStatus` for handling final statuses and state checks

--- app/Enums/KeywordStatus.php
+++ app/Enums/KeywordStatus.php
@@ -8,4 +8,14 @@ enum KeywordStatus: int
     case RESEARCHING = 2;
     case RESEARCHED = 3;
     case ERROR = 4;
+
+    public static function finalStatuses(): array
+    {
+        return [self::RESEARCHED, self::ERROR];
+    }
+
+    public function isFinal(): bool
+    {
+        return in_array($this, self::finalStatuses());
+    }
 }

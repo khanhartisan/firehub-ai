367c31866b20f49bbb5644d212644c0c0ef3f99b
khanh.artisan <khanh.artisan@gmail.com>
2026-04-27 14:54:57 +0700

    add `CancelIgnoreScrapingBudget` model listener for handling page saving events

---
+++ app/ModelListeners/Page/Saving/CancelIgnoreScrapingBudget.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\ModelListeners\Page\Saving;
+
+use App\Models\Page;
+use KhanhArtisan\LaravelBackbone\ModelListener\ModelListener;
+use KhanhArtisan\LaravelBackbone\ModelListener\ModelListenerInterface;
+
+class CancelIgnoreScrapingBudget extends ModelListener implements ModelListenerInterface
+{
+    /**
+     * Listeners with higher priority will run first.
+     *
+     * @return int
+     */
+    public function priority(): int
+    {
+        return 0;
+    }
+
+    /**
+     * Listen to the events of the given model.
+     *
+     * @return string
+     */
+    public function modelClass(): string
+    {
+        return Page::class;
+    }
+
+    /**
+     * The list of all the events to listen to.
+     *
+     * @return array<string>
+     */
+    public function events(): array
+    {
+        return ["saving"];
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param Page $page
+     * @param string $event
+     * @return void
+     */
+    protected function _handle(Page $page, string $event): void
+    {
+        if ($page->attempts < config('queue.max_scrape_attempts')) {
+            return;
+        }
+
+        $page->ignore_scraping_budget = false;
+    }
+}

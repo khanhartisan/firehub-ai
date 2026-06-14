2c28c5baa080c4c699f167eed479eee0ef49417b
khanh.artisan <khanh.artisan@gmail.com>
2026-06-14 14:22:15 +0700

    update PublicationStatus.php

--- app/Enums/PublicationStatus.php
+++ app/Enums/PublicationStatus.php
@@ -2,11 +2,29 @@
 
 namespace App\Enums;
 
-enum PublicationStatus: int {
-    case PENDING = 0;
-    case PUBLISHING = 1;
-    case PUBLISHED = 2;
-    case TIMEOUT = 3;
-    case FAILED = 4;
-    case ERROR = 5;
+use App\Contracts\DescribableEnum;
+
+enum PublicationStatus: int implements DescribableEnum
+{
+    case AWAITING = 0;
+    case PENDING = 1;
+    case PUBLISHING = 2;
+    case PUBLISHED = 3;
+    case TIMEOUT = 4;
+    case FAILED = 5;
+    case ERROR = 6;
+
+    public static function describe(DescribableEnum $enum): string
+    {
+        return match ($enum) {
+            static::AWAITING => 'Awaiting for the base resource to be ready',
+            static::PENDING => 'The base resource is ready, awaiting for publishing',
+            static::PUBLISHING => 'In publishing process',
+            static::PUBLISHED => 'Published successfully',
+            static::TIMEOUT => 'Publishing process is timed out',
+            static::FAILED => 'Failed to publish',
+            static::ERROR => 'Got error during the publishing process',
+            default => 'Unknown',
+        };
+    }
 }

ee7f24556dca133aeb88d3be32a7e8dfcc9ed1ff
khanh.artisan <khanh.artisan@gmail.com>
2026-06-24 17:42:25 +0700

    add Sentry integration to Laravel app and update dependencies accordingly

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,5 +15,5 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        \Sentry\Laravel\Integration::handles($exceptions);
     })->create();

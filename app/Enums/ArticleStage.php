2e9e336114e0c14d085973e10ccffc9116a4f7b6
khanh.artisan <khanh.artisan@gmail.com>
2026-07-04 15:26:17 +0700

    refactor: reorder `ArticleStage` enum cases to adjust stage sequencing

--- app/Enums/ArticleStage.php
+++ app/Enums/ArticleStage.php
@@ -7,10 +7,10 @@ enum ArticleStage: int
     case IDEA = 1;
     case RESEARCH = 2;
     case BRIEF = 3;
-    case OUTLINE = 4;
-    case DRAFT = 5;
-    case RECTIFICATION = 6;
-    case ILLUSTRATION = 7;
-    case FINAL = 8;
-    case TAGGING = 9;
+    case TAGGING = 4;
+    case OUTLINE = 5;
+    case DRAFT = 6;
+    case RECTIFICATION = 7;
+    case ILLUSTRATION = 8;
+    case FINAL = 9;
 }
